e26a5f34942586632e91b1749adf53176cbffae9
Ryan Joseph <ryan.joseph@gmail.com>
2020-07-18 16:34:56 -0700

    Skip ioredis test when NODE_ENV='ci'; fixup package.json

--- test/ioredis.js
+++ test/ioredis.js
@@ -25,52 +25,61 @@ const execWithLocal = async (eFunc) => {
 const singleLocal = async (t, x, y) => 
   execWithLocal(async (bitmap) => shared.singleSetTestUnset('rLocal', t, x, y, bitmap));
 
-test('construct with lazyConnect ioredis instance', function (t) {
-  t.plan(2);
-  
-  t.doesNotThrow(function () {
-    const bitmap = new TwoD.SparseBitmap({
-      [TwoD.BackingStoreKey]: new Redis({ lazyConnect: true })
+function main() {
+  if ('NODE_ENV' in process.env && process.env.NODE_ENV === 'ci') {
+    console.log('*** Skipping ALL ioredis tests in CI environment ***\n');
+    return;
+  }
+
+  test('construct with lazyConnect ioredis instance', function (t) {
+    t.plan(2);
+    
+    t.doesNotThrow(function () {
+      const bitmap = new TwoD.SparseBitmap({
+        [TwoD.BackingStoreKey]: new Redis({ lazyConnect: true })
+      });
+
+      t.true(bitmap.isPipelineCapable);
     });
-
-    t.true(bitmap.isPipelineCapable);
   });
-});
 
-test('local redis - 0,0', async function (t) { await singleLocal(t, 0, 0); });
-test('local redis - 0,1', async function (t) { await singleLocal(t, 0, 1); });
-test('local redis - 1,0', async function (t) { await singleLocal(t, 1, 0); });
-test('local redis - 1,1', async function (t) { await singleLocal(t, 1, 1); });
+  test('local redis - 0,0', async function (t) { await singleLocal(t, 0, 0); });
+  test('local redis - 0,1', async function (t) { await singleLocal(t, 0, 1); });
+  test('local redis - 1,0', async function (t) { await singleLocal(t, 1, 0); });
+  test('local redis - 1,1', async function (t) { await singleLocal(t, 1, 1); });
 
-test('local redis - 42 random, non-strict', async function (t) {
-  await execWithLocal(async (bitmap) => shared.setNRandomAndCheckInBounds('r42Rand', t, 42, bitmap));
-});
-
-test('local redis - random random, non-strict', async function (t) {
-  const nRand = Math.floor(512 + Math.random() * 512);
-  console.log(`using n=${nRand}`);
-  await execWithLocal(async (bitmap) => shared.setNRandomAndCheckInBounds(`rRand${nRand}Rand`, t, nRand, bitmap));
-});
-
-test('local redis - 42 random, strict', async function (t) {
-  await execWithLocal(async (bitmap) => shared.setNRandomAndCheckInBounds('r42Rand', t, 42, bitmap, true));
-});
-
-test('local redis - random random, strict', async function (t) {
-  const nRand = Math.floor(512 + Math.random() * 512);
-  console.log(`using n=${nRand}`);
-  await execWithLocal(async (bitmap) => shared.setNRandomAndCheckInBounds(`rRand${nRand}Rand`, t, nRand, bitmap, true));
-});
-
-test('local redis pipeline compare - default', async function (t) {
-  await execWithLocal(async (bitmap) => {
-    return shared.setNRandomAndCheckInBounds(`421`, t, 421, bitmap, true);
+  test('local redis - 42 random, non-strict', async function (t) {
+    await execWithLocal(async (bitmap) => shared.setNRandomAndCheckInBounds('r42Rand', t, 42, bitmap));
   });
-});
 
-test('local redis pipeline compare - NO pipeline', async function (t) {
-  await execWithLocal(async (bitmap) => {
-    bitmap.isPipelineCapable = false;
-    return shared.setNRandomAndCheckInBounds(`421-NP`, t, 421, bitmap, true);
+  test('local redis - random random, non-strict', async function (t) {
+    const nRand = Math.floor(512 + Math.random() * 512);
+    console.log(`using n=${nRand}`);
+    await execWithLocal(async (bitmap) => shared.setNRandomAndCheckInBounds(`rRand${nRand}Rand`, t, nRand, bitmap));
   });
-});
+
+  test('local redis - 42 random, strict', async function (t) {
+    await execWithLocal(async (bitmap) => shared.setNRandomAndCheckInBounds('r42Rand', t, 42, bitmap, true));
+  });
+
+  test('local redis - random random, strict', async function (t) {
+    const nRand = Math.floor(512 + Math.random() * 512);
+    console.log(`using n=${nRand}`);
+    await execWithLocal(async (bitmap) => shared.setNRandomAndCheckInBounds(`rRand${nRand}Rand`, t, nRand, bitmap, true));
+  });
+
+  test('local redis pipeline compare - default', async function (t) {
+    await execWithLocal(async (bitmap) => {
+      return shared.setNRandomAndCheckInBounds(`421`, t, 421, bitmap, true);
+    });
+  });
+
+  test('local redis pipeline compare - NO pipeline', async function (t) {
+    await execWithLocal(async (bitmap) => {
+      bitmap.isPipelineCapable = false;
+      return shared.setNRandomAndCheckInBounds(`421-NP`, t, 421, bitmap, true);
+    });
+  });
+}
+
+main();
